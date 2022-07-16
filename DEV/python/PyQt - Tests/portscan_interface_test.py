a26400b152a49b5675aae68d17b1f51f1106a478
DosBoss <w0lfheart@pm.me>
2022-07-17 03:01:14 +0800

    Lazy module solution to test GUI. plus Art

--- DEV/python/PyQt - Tests/portscan_interface_test.py
+++ DEV/python/PyQt - Tests/portscan_interface_test.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+import sts
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -12,9 +12,10 @@ class Ui_MainWindow(object):
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(420, 160, 81, 31))
         self.pushButton.setAutoFillBackground(False)
-        self.pushButton.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+        self.pushButton.setStyleSheet("background-color: rgb(40, 40, 40);\n"
                                         "border-color: rgb(38, 190, 0);\n"
-                                        "gridline-color: rgb(46, 156, 60);")
+                                        "gridline-color: rgb(46, 156, 60);\n"
+                                        "color: rgb(255, 255, 255);")
         self.pushButton.setObjectName("pushButton")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(130, 0, 371, 151))
@@ -75,13 +76,14 @@ class Ui_MainWindow(object):
                                 "p, li { white-space: pre-wrap; }\n"
                                 "hr { height: 1px; border-width: 0; }\n"
                                 "</style></head><body style=\" font-family:\'Segoe UI\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-                                "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" color:#26be00;\">© SysTrix 2022</span></p></body></html>")
+                                "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" color:#26be00;\">© 2022 SysTrix</span></p></body></html>")
         self.label_2.setTextFormat(QtCore.Qt.RichText)
         self.label_2.setScaledContents(False)
         self.label_2.setObjectName("label_2")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(130, 160, 281, 31))
-        self.lineEdit.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.lineEdit.setStyleSheet("background-color: rgb(30,30,30);\n"
+                                    "color: rgb(255, 255, 255);")
         self.lineEdit.setObjectName("lineEdit")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -94,8 +96,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "SysTrixSuite - Trickology Evolved"))
-        self.pushButton.setText(_translate("MainWindow", "Initiate Scan"))
-        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Enter target (x.x.x.x/x)"))
+        self.pushButton.setText(_translate("MainWindow", "Scan"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Enter target (IP/Subnet or URL)"))
 
 def loadMainWindow():
     app = QtWidgets.QApplication(sys.argv)
@@ -111,6 +113,9 @@ def suppress_qt_warnings():
     environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
     environ["QT_SCREEN_SCALE_FACTORS"] = "1"
     environ["QT_SCALE_FACTOR"] = "1"
+
+
+
 
 
 if __name__ == "__main__":
---
+++ DEV/python/PyQt - Tests/sts.py
@@ -0,0 +1,69 @@
+# Re-dumps of the "Modules" scripts
+# Coz I'm tired af, and dont wanna make things class's rn
+# fuck you, sue me <3
+
+import socket
+import time
+import threading
+from queue import Queue
+from IPy import IP
+
+targets = 0
+port_range = 1024
+
+thread_count = 1024
+print_lock = threading.Lock()
+q = Queue()
+
+
+def checkIP(ip):
+    try:
+        IP(ip)
+        return(ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+
+def threader():
+    while True:
+        worker = q.get()
+        portscan(worker)
+        q.task_done()
+    return
+
+def portscan(port):
+    scan_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    banner_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    banner_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    banner_sock.settimeout(1)
+    banner = ""
+    try:
+        con = scan_sock.connect((targets, port))
+            
+        #with print_lock:
+        try:
+            banner_con = banner_sock.connect((targets, port))
+            banner = banner_sock.recv(1024).decode().strip("\n").strip("\r")
+            banner_con.close()
+        except Exception:
+            pass 
+        print("[*] Port " + str(port) + " is open" + ": " + banner)
+        con.close()    
+    except Exception:
+        pass
+
+def runScan():
+    for x in range(thread_count):
+        t = threading.Thread(target=threader)
+        t.daemon = True
+        t.start()
+
+    start = time.time()
+    for worker in range(1, port_range + 1):
+        q.put(worker)
+    q.join()
+    end = time.time()
+
+    print("[-] Scanned " + str(port_range) + " ports in " + str(round(end - start)) + " seconds")
+    quit()
+
