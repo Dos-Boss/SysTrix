95528b79ff92923e315f46eb6825fc670a78c80e
DosBoss <w0lfheart@pm.me>
2022-07-16 20:36:08 +0800

    Locked resizing of MainWindow

--- DEV/python/PyQt - Tests/portscan_interface_test.py
+++ DEV/python/PyQt - Tests/portscan_interface_test.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(620, 496)
+        MainWindow.resize(620, 460)
         MainWindow.setAutoFillBackground(False)
         MainWindow.setStyleSheet("background-color: rgb(40, 40, 40);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -46,7 +46,6 @@ class Ui_MainWindow(object):
                                         "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\"> /        \\___  |\\___ \\  |    |   |  | \\/  |&gt;    &lt; </span></p>\n"
                                         "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">/_______  / ____/____  &gt; |____|   |__|  |__/__/\\_ \\</span></p>\n"
                                         "<p align=\"center\" style=\" margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">        \\/\\/         \\/                          \\/</span></p>\n"
-                                        "<p align=\"center\" style=\" margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'sans-serif\'; font-size:14px; font-weight:700; color:#26be00;\">© </span><span style=\" font-family:\'Courier New\'; color:#26be00;\">SysTrix 2022</span></p>\n"
                                         "<p align=\"center\" style=\" margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">System Trickology - Evolved.</span></p></body></html>")
         self.label.setTextFormat(QtCore.Qt.RichText)
         self.label.setScaledContents(False)
@@ -56,15 +55,23 @@ class Ui_MainWindow(object):
         self.textBrowser.setStyleSheet("background-color: rgb(55, 55, 55);")
         self.textBrowser.setAcceptRichText(True)
         self.textBrowser.setObjectName("textBrowser")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(0, 440, 81, 21))
+        self.label_2.setStyleSheet("")
+        self.label_2.setText("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+                                "p, li { white-space: pre-wrap; }\n"
+                                "hr { height: 1px; border-width: 0; }\n"
+                                "</style></head><body style=\" font-family:\'Segoe UI\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+                                "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" color:#26be00;\">© SysTrix 2022</span></p></body></html>")
+        self.label_2.setTextFormat(QtCore.Qt.RichText)
+        self.label_2.setScaledContents(False)
+        self.label_2.setObjectName("label_2")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 620, 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
@@ -81,5 +88,6 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    MainWindow.setFixedSize(MainWindow.size())
     MainWindow.show()
     sys.exit(app.exec_())
