7bd84e8b2c8b519aff3595fc61ae8f4b03bb32ba
DosBoss <w0lfheart@pm.me>
2022-07-16 20:55:02 +0800

    Copyright info

--- DEV/python/PyQt - Tests/portscan_interface_test.py
+++ DEV/python/PyQt - Tests/portscan_interface_test.py
@@ -14,17 +14,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(620, 460)
+        MainWindow.resize(620, 484)
         MainWindow.setAutoFillBackground(False)
-        MainWindow.setStyleSheet("background-color: rgb(40, 40, 40);")
+        MainWindow.setStyleSheet("background-color: rgb(30, 30, 30);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(130, 160, 281, 31))
-        self.textEdit.setStyleSheet("background-color: rgb(255, 255, 255);\n"
-                                        "border-color: rgb(38, 190, 0);\n"
-                                        "gridline-color: rgb(46, 156, 60);")
-        self.textEdit.setObjectName("textEdit")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(420, 160, 81, 31))
         self.pushButton.setAutoFillBackground(False)
@@ -36,27 +30,29 @@ class Ui_MainWindow(object):
         self.label.setGeometry(QtCore.QRect(130, 0, 371, 151))
         self.label.setStyleSheet("")
         self.label.setText("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                        "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-                                        "p, li { white-space: pre-wrap; }\n"
-                                        "hr { height: 1px; border-width: 0; }\n"
-                                        "</style></head><body style=\" font-family:\'Segoe UI\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-                                        "<p align=\"center\" style=\" margin-top:12px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">  _________            ___________      .__        </span></p>\n"
-                                        "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\"> /   _____/__.__. _____\\__    ___/______|__|__  ___</span></p>\n"
-                                        "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\"> \\_____  &lt;   |  |/  ___/ |    |  \\_  __ \\  \\  \\/  /</span></p>\n"
-                                        "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\"> /        \\___  |\\___ \\  |    |   |  | \\/  |&gt;    &lt; </span></p>\n"
-                                        "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">/_______  / ____/____  &gt; |____|   |__|  |__/__/\\_ \\</span></p>\n"
-                                        "<p align=\"center\" style=\" margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">        \\/\\/         \\/                          \\/</span></p>\n"
-                                        "<p align=\"center\" style=\" margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">System Trickology - Evolved.</span></p></body></html>")
+                                "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+                                "p, li { white-space: pre-wrap; }\n"
+                                "hr { height: 1px; border-width: 0; }\n"
+                                "</style></head><body style=\" font-family:\'Segoe UI\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+                                "<p align=\"center\" style=\" margin-top:12px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">  _________            ___________      .__        </span></p>\n"
+                                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\"> /   _____/__.__. _____\\__    ___/______|__|__  ___</span></p>\n"
+                                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\"> \\_____  &lt;   |  |/  ___/ |    |  \\_  __ \\  \\  \\/  /</span></p>\n"
+                                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\"> /        \\___  |\\___ \\  |    |   |  | \\/  |&gt;    &lt; </span></p>\n"
+                                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">/_______  / ____/____  &gt; |____|   |__|  |__/__/\\_ \\</span></p>\n"
+                                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">        \\/\\/         \\/                          \\/</span></p>\n"
+                                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; color:#26be00;\">System Trickology - Evolved.</span></p></body></html>")
         self.label.setTextFormat(QtCore.Qt.RichText)
         self.label.setScaledContents(False)
         self.label.setObjectName("label")
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
         self.textBrowser.setGeometry(QtCore.QRect(130, 200, 371, 231))
-        self.textBrowser.setStyleSheet("background-color: rgb(55, 55, 55);")
+        self.textBrowser.setStyleSheet("background-color: rgb(40, 40, 40);\n"
+                                        "selection-background-color: rgb(255, 0, 0);\n"
+                                        "gridline-color: rgb(255, 0, 0);")
         self.textBrowser.setAcceptRichText(True)
         self.textBrowser.setObjectName("textBrowser")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(0, 440, 81, 21))
+        self.label_2.setGeometry(QtCore.QRect(0, 460, 81, 21))
         self.label_2.setStyleSheet("")
         self.label_2.setText("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
                                 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
@@ -67,6 +63,10 @@ class Ui_MainWindow(object):
         self.label_2.setTextFormat(QtCore.Qt.RichText)
         self.label_2.setScaledContents(False)
         self.label_2.setObjectName("label_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(130, 160, 281, 31))
+        self.lineEdit.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.lineEdit.setObjectName("lineEdit")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 620, 22))
@@ -78,12 +78,10 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "SysTrixSuite - Trickology Evolved"))
-        self.textEdit.setPlaceholderText(_translate("MainWindow", "Enter target (xx.xx.xx.xx/xx)"))
         self.pushButton.setText(_translate("MainWindow", "Initiate Scan"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Enter target (xx.xx.xx.xx/xx)"))
 
-
-if __name__ == "__main__":
-    import sys
+def loadMainWindow():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
@@ -91,3 +89,9 @@ if __name__ == "__main__":
     MainWindow.setFixedSize(MainWindow.size())
     MainWindow.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    import sys
+
+loadMainWindow()
